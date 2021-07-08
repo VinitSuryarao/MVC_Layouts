4506d9d856e8cf5d7d6e98b47a943e474b7fe1e4
Vinit Suryarao <suryarao.vinit@hotmail.com>
2021-07-08 19:25:21 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Layout.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Client")]
+        public ActionResult GetClient()
+        {
+            return View();
+        }
+
+        [Route("Exchange")]
+        public ActionResult GetExchange()
+        {
+            return View("GetExchange", "_LayoutHeaderFooter");
+        }
+    }
+}
